ed64fda7aef66793c2b0f40ee89dc20a4a1f6837
Daven <davensteen@gmail.com>
2022-08-30 23:01:42 -0500

    password length prompt

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Prompt the user
+function generatePassword() {
+  var inputPasswordLength = window.prompt("Choose your length of password between 8 to 128 characters!")
+  var passwordLength = parseInt(inputPasswordLength)
+
+  if (isNaN(passwordLength) || passwordLength<8 || passwordLength > 128) {
+    window.alert ("Enter a number between 8 and 128!")
+    return
+  } 
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
