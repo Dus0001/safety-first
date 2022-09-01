bafb9101cc284251e9fe5266f96e32292b4faabe
Daven <davensteen@gmail.com>
2022-08-31 23:49:47 -0500

    added the script to btn and fixed bugs

--- Develop/script.js
+++ Develop/script.js
@@ -2,23 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 
 var randomInt = function(min,max) {
-  var value =cMath.floor(Math.random() * (max - min) + min)
-  
-  return value;
+  if (!max) {
+    max - min
+    min = 0
+  }
+  var rand = Math.random()
+  return Math.floor(min*(1-rand) + rand * max)
 };
+
+function getRandomItem(list) {
+  return list[randomInt(0, list.length)]
+}
 
 //Prompt the user
 function generatePassword() {
   
   
   var numbersArray = ["0","1","2","3","4","5","6","7","8","9"]
-  var charactersArray = ["!","@","#","$","%","&","*","?"]
+  var symbolsArray = ["!","@","#","$","%","&","*","?"]
   var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-  var lowerCase = []
-  var passwordInfo = [];
-  for (var i = 0; i < upperCase.length; i++ ) {
-    lowerCase[i] = upperCase[i].toLowerCase()
-  }
+  var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+  var passwordInfo = []
+
   var inputPasswordLength = window.prompt("Choose your length of password between 8 to 128 characters!")
   var passwordLength = parseInt(inputPasswordLength)
 
@@ -29,31 +34,38 @@ function generatePassword() {
   }
   //Lower case option
   var inputLowerCase = window.confirm("Do you want lower case letters?")
-  if (inputLowerCase) {
-    passwordInfo.push(inputLowerCase)
+  if (inputLowerCase === true) {
+    passwordInfo.push(lowerCase)
   }
 
   //Upper case option 
   var inputUpperCase = window.confirm("Do you want UPPER case letters?")
-  if (inputUpperCase) {
+  if (inputUpperCase === true) {
     passwordInfo.push(upperCase)
   }
   
   //numbers option
   var inputNumbers = window.confirm("Do you want to add numbers?")
-  if (inputNumbers) {
+  if (inputNumbers === true) {
     passwordInfo.push(numbersArray)
   }
 
   //Character options
-  var inputCharacters = window.confirm("Do you want to add characters?")
-  if (inputCharacters) {
-    passwordInfo.push(charactersArray)
+  var inputSymbols = window.confirm("Do you want to add symbols?")
+  if (inputSymbols === true) {
+    passwordInfo.push(symbolsArray)
+  };
+
+  //connect Formulated password to the button
+  var formulatedPassword = ""
+
+  for (i = 0; i < passwordLength; i++) {
+  var desiredOptions = getRandomItem(passwordInfo)
+  var randomScript = getRandomItem(desiredOptions)
+    formulatedPassword += randomScript
   }
 
-  //connect
-
-
+  return formulatedPassword
 };
 
 
@@ -62,7 +74,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value += password;
 
 }
 
